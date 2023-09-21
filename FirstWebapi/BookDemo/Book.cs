64a0d1ed86c66ccd7a7d3ad1e116ae85c25cb913
omkaarrudra357 <saurabh.kumar@gavstech.com>
2023-09-21 14:14:48 +0530

    insidegit

---
+++ FirstWebapi/BookDemo/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstWebapi.BookDemo
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
