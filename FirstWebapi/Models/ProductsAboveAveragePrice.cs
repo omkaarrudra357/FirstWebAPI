5b8b3602013e3ad0bf1474d97539829b147f15a7
omkaarrudra357 <saurabh.kumar@gavstech.com>
2023-09-23 10:17:25 +0530

    Employee commit

---
+++ FirstWebapi/Models/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstWebapi.Models;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}
--- FirstWebapi/Program.cs
+++ FirstWebapi/Program.cs
@@ -1,13 +1,35 @@
+using FirstWebapi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 
+
+
+
+
+builder.Services.AddControllersWithViews().AddJsonOptions(options => { options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve; });
+builder.Services.AddControllersWithViews().AddJsonOptions(options => { options.JsonSerializerOptions.MaxDepth = int.MaxValue; });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer("name=NWindConnection"));
+builder.Services.AddScoped<RepositoryEmployee>();
+
+
+
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,8 +38,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseAuthorization();
 
+
+
 app.MapControllers();
+
+
 
 app.Run();
